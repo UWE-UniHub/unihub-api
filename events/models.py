6130b4503a81b49c993471b2305d24a2b5901717
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2025-02-22 15:58:04 +0000

    def followers are counting the numbers of followers of each profile(user)

--- events/models.py
+++ events/models.py
@@ -7,6 +7,6 @@ class Event(models.Model):
     location = models.CharField(max_length=255)
     date = models.DateTimeField()
     creator = models.ForeignKey('profiles.Profile', on_delete=models.CASCADE, related_name='created_events')
-    community = models.ForeignKey('communities.Community', on_delete=models.CASCADE, null=True, blank=True, related_name='events') # Добавлено related_name
+    community = models.ForeignKey('communities.Community', on_delete=models.CASCADE, null=True, blank=True, related_name='events')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
