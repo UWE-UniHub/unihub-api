9e28ac5b380c657641388191a1ec2f56f00e8055
Nuraly <kracken787@gmail.com>
2025-02-24 19:26:12 +0000

    email removed and a lot of fixes

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,6 +2,34 @@ from rest_framework import serializers
 from profiles.models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
+    subscribers = serializers.SerializerMethodField()
+    subscriptions = serializers.SerializerMethodField()
+    student = serializers.SerializerMethodField()
+    staff = serializers.SerializerMethodField()
+
     class Meta:
         model = Profile
-        fields = ['id', 'email', 'first_name', 'last_name', 'bio', 'is_staff'] # add student or staff later, idk how to do it now
+        fields = ['id', 'first_name', 'last_name', 'bio', 'is_staff', 'subscribers', 'subscriptions', 'student', 'staff']
+
+    def get_subscribers(self, obj):
+        return obj.subscribers.count()
+
+    def get_subscriptions(self, obj):
+        return obj.subscriptions.count()
+
+    def get_student(self, obj):
+        if not obj.is_staff:
+            return {
+                "program": obj.program,
+                "level": obj.level,
+                "school": obj.school
+            }
+        return None
+
+    def get_staff(self, obj):
+        if obj.is_staff:
+            return {
+                "position": obj.position,
+                "department": obj.department
+            }
+        return None
