a8a2203f957eead0a38cacf0dd783f9e09b5dc9c
Ilyas Utenov <117728333+0dreadnought@users.noreply.github.com>
2025-02-26 05:19:37 +0500

    /auth DELETE(logout_view) (#17)
    Co-authored-by: KaluginArtem <102584306+KaluginArtem@users.noreply.github.com>

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_view, signup_view, auth_check
+from .views import login_view, signup_view, auth_check, logout_view
 
 urlpatterns = [
     path('auth/login/', login_view, name='login'),
     path('auth/signup/', signup_view, name='signup'),
     path('auth/', auth_check, name='auth_check'),
+    path('auth/logout/', logout_view, name='logout'),
 ]
