27a50a41fd31c7151b1fb038890f951ab4096fe8
Nuraly <116110183+baktygaliyevv@users.noreply.github.com>
2025-02-28 15:23:26 +0000

    urls fixes with slash (#33)

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import profile_detail
 
 urlpatterns = [
-    path('<int:id>/', profile_detail, name='profile_detail'),
+    path('<int:id>', profile_detail, name='profile_detail'),
 ]
