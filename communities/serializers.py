44b5b69408f48ce07917372db174ffa377556a2c
KaluginArtem <102584306+KaluginArtem@users.noreply.github.com>
2025-03-03 14:35:00 +0000

    now shows creator and admins

--- communities/serializers.py
+++ communities/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from communities.models import Community
+from profiles.serializers import ProfileSerializer
 
 class CommunitySerializer(serializers.ModelSerializer):
     subscribers = serializers.SerializerMethodField()
-    
+    creator = ProfileSerializer()
+    admins = ProfileSerializer(many=True)
 
     class Meta:
         model = Community
-        fields = ['id', 'name', 'bio', 'subscribers']
+        fields = ['id', 'name', 'bio', 'creator', 'admins','subscribers']
 
     def get_subscribers(self, obj):
         return obj.subscribers.count()
