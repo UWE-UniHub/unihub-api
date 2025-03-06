99ac855ff6fdd8ef862485a124fc280a10a91bf1
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2025-03-06 23:59:12 +0000

    Update utils.py

--- unihub/utils.py
+++ unihub/utils.py
@@ -3,9 +3,9 @@ from io import BytesIO
 
 def validate_png(file_bytes):
     try:
-        image = Image.open(BytesIO(file_bytes))
-        if image.format != 'PNG':
-            return False, "Uploaded file is not a valid PNG image"
+        with Image.open(BytesIO(file_bytes)) as image:
+            if image.format != 'PNG':
+                return False, "Uploaded file is not a valid PNG image."
         return True, None
     except Exception as e:
         return False, f"Error processing image: {str(e)}"
