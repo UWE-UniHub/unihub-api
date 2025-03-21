15d4199d07a98a5cb2229c795241712e51e802bc
KaluginArtem <102584306+KaluginArtem@users.noreply.github.com>
2025-03-21 09:38:37 +0000

    Get eligible admins for community
    closes #56

--- communities/views.py
+++ communities/views.py
@@ -166,3 +166,10 @@ def add_delete_admin(request, community_id, admin_id):
         delete_admin = get_object_or_404(Profile, id=admin_id)
         community.admins.remove(delete_admin)
         return Response({"message": "Admin removed successfully"}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def get_eligible_admins(request, id):
+    community = get_object_or_404(Community, id=id)
+    possible_admins = Profile.objects.exclude(id__in=community.admins.all()).exclude(id=community.creator.id)
+    admins_serializer = ProfileSerializer(possible_admins, many=True)
+    return Response(admins_serializer.data, status=status.HTTP_200_OK)
