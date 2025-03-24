a36ee22a0349d9500e29212fcee5550630c70920
Ilyas Utenov <117728333+0dreadnought@users.noreply.github.com>
2025-03-24 20:20:49 +0000

    add_postslikes_model (#58)
    * add_postslikes
    
    * likes support
    
    likes displaaeyed
    Get users ho liked
    Post Like
    Delete like
    
    ---------
    
    Co-authored-by: Nuraly <116110183+baktygaliyevv@users.noreply.github.com>

--- posts/models.py
+++ posts/models.py
@@ -11,6 +11,21 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    likes = models.ManyToManyField(
+        'profiles.Profile',
+        through='PostLikes',
+        related_name='like_posts',
+        blank=True
+    )
+
     def clean(self):
         if (self.profile is None and self.community is None) or (self.profile is not None and self.community is not None):
             raise ValidationError("Post must have either a profile or a community author, but not both.")
+        
+class PostLikes(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    profile = models.ForeignKey('profiles.Profile', on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('post','profile')
+        
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import postsIdGetPatchDelete, postIdImgGetPutDelete
+from .views import postsIdGetPatchDelete, postIdImgGetPutDelete, postIdLikesGetPostDelete
 
 urlpatterns = [
     path('/<str:id>', postsIdGetPatchDelete, name='postsIdGetPatchDelete'),
     path('/<str:id>/img', postIdImgGetPutDelete, name='postIdImgGetPutDelete'),
+    path('/<str:id>/likes', postIdLikesGetPostDelete, name='postIdLikesGetPostDelete'),
 ]
