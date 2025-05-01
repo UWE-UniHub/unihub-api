479f82f12805aded6fb8eaa53f27a043a942c665
Nuraly Baktygaliyev <116110183+baktygaliyevv@users.noreply.github.com>
2025-05-01 19:29:53 +0100

    events subscriptions and new fields (#81)

--- events/models.py
+++ events/models.py
@@ -8,5 +8,20 @@ class Event(models.Model):
     date = models.DateTimeField()
     creator = models.ForeignKey('profiles.Profile', on_delete=models.CASCADE, related_name='created_events')
     community = models.ForeignKey('communities.Community', on_delete=models.CASCADE, null=True, blank=True, related_name='events')
+    required_materials = models.TextField(blank=True, null=True)
+    max_capacity = models.PositiveIntegerField(null=True, blank=True)
+    subscribers = models.ManyToManyField(
+        'profiles.Profile',
+        through='EventSubscription',
+        related_name='subscribed_events',
+        blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class EventSubscription(models.Model):
+    profile = models.ForeignKey('profiles.Profile', on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('profile', 'event')
