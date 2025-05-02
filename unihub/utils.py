26f777a4d5c32036d0f966a95ab42518ba4faa18
baktygaliyevv <116110183+baktygaliyevv@users.noreply.github.com>
2025-05-02 20:04:06 +0100

    Update utils.py

--- unihub/utils.py
+++ unihub/utils.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
 
 class FreemiumPagination(PageNumberPagination):
-    default_limit = 10
+    default_limit = 30
     page_query_param = 'page'
  
     def get_paginated_response(self, data):
