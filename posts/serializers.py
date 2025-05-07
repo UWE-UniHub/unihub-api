6010274ce001d943e6e056e18beeaabfe753137b
Nuraly Baktygaliyev <116110183+baktygaliyevv@users.noreply.github.com>
2025-05-07 17:23:26 +0100

    Update serializers.py (#109)

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from posts.views import check_user_can_pD_posts
 from .models import Post
 from communities.serializers import CommunitySerializer
 from profiles.serializers import ProfileSerializer
@@ -13,11 +14,12 @@ class PostSerializer(serializers.ModelSerializer):
 
     likes = serializers.SerializerMethodField()
     is_liked = serializers.SerializerMethodField()
+    is_editable = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'content', 'event','profile','community', 'tags', 'hidden', 'created_at','likes','is_liked']
-        read_only_fields = ['id', 'created_at','community','profile','likes','is_liked']
+        fields = ['id', 'content', 'event', 'profile', 'community', 'tags', 'hidden', 'created_at','likes', 'is_liked', 'is_editable']
+        read_only_fields = ['id', 'created_at', 'community', 'profile', 'likes', 'is_liked', 'is_editable']
 
     def get_likes(self,obj):
         return obj.likes.count()
@@ -25,6 +27,12 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_liked(self,obj):
         user = serializers_get_user_from_request(self)
         return user.is_authenticated and obj.likes.filter(id=user.id).exists() if user else False
+    
+    def get_is_editable(self, obj):
+        user = serializers_get_user_from_request(self)
+        if not user or not user.is_authenticated:
+            return False
+        return check_user_can_pD_posts(user, obj)
 
 class PostPostSerializer(serializers.ModelSerializer):
     class Meta:
